e0067804b9a307dab24deef79f4bcf27a9c860f6
tombolton96 <tbolton96@icloud.com>
2024-08-13 16:32:06 +0100

    add observer hoc to RecitationInfo

--- components/RecitationInfo.tsx
+++ components/RecitationInfo.tsx
@@ -3,30 +3,31 @@ import { ThemedView } from '@/components/ThemedView'
 import { ThemedText } from '@/components/ThemedText'
 import { NowPlayingStore } from '@/globalState/store'
 import { Suwar } from '@/constants/Suwar'
+import { observer } from 'mobx-react'
 
 type RecitationInfoProps = {
   isWidget?: boolean
   nowPlaying: NowPlayingStore
 }
 
-export const RecitationInfo = (props: RecitationInfoProps) => {
+export const RecitationInfo = observer((props: RecitationInfoProps) => {
   return (
     <ThemedView style={props.isWidget ? styles.widgetContainer : styles.playerViewContainer}>
       <ThemedText>
         {!props.nowPlaying.isLoading
           ? Suwar[props.nowPlaying.surahNumber - 1].name
-          : '-'
+          : '-' // TODO: loading component
         }
       </ThemedText>
       <ThemedText>
         {!props.nowPlaying.isLoading
           ? props.nowPlaying.currentReciter?.name
-          : '-'
+          : '-' // TODO: loading component
         }
       </ThemedText>
     </ThemedView>
   )
-}
+})
 
 const styles = StyleSheet.create({
   playerViewContainer: {
