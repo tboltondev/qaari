bb5bf51ae9c5841ac93b6c5b5f56667a9f064fbd
tombolton96 <tbolton96@icloud.com>
2024-08-17 11:38:08 +0100

    modify home page icons

--- app/index.tsx
+++ app/index.tsx
@@ -6,46 +6,71 @@ import { ThemedView } from '@/components/ThemedView'
 import { MenuItem } from '@/components/MenuItem'
 import { useThemeColor } from '@/hooks/useThemeColor'
 import { Menu } from '@/components/Menu'
+import { SimpleLineIcons } from '@expo/vector-icons'
 
 export default function HomeScreen () {
   const textColor = useThemeColor({}, 'text')
-  const disabledTextColor = useThemeColor({}, 'disabled')
+  const secondaryTextColor = useThemeColor({}, 'secondaryText')
 
   const menuItems: MenuItem[] = [
     {
       title: 'Reciters',
       href: '/reciters',
-      icon: <Ionicons name='mic' size={30} color={textColor} style={{ marginEnd: 20 }} />,
+      icon: (
+        <ThemedView style={[styles.mainMenuItemIcon, { backgroundColor: 'limegreen' }]}>
+          <Ionicons name="mic-outline" size={20} color={'white'} />
+        </ThemedView>
+      ),
+      endIcon: <SimpleLineIcons name="arrow-right" size={15} color={secondaryTextColor}
+                                style={styles.mainMenuItemEndIcon}/>,
     },
     {
       title: 'Playlists',
       href: '/playlists',
-      icon: <Ionicons name='list' size={30} color={textColor} style={{ marginEnd: 20 }} />,
+      icon: (
+        <ThemedView style={[styles.mainMenuItemIcon, { backgroundColor: 'grey' }]}>
+          <Ionicons name="list-outline" size={20} color={'white'} />
+        </ThemedView>
+      ),
+      endIcon: <SimpleLineIcons name="arrow-right" size={15} color={secondaryTextColor}
+                                style={styles.mainMenuItemEndIcon}/>,
     },
     {
       title: 'Qira\'at',
       href: '/qiraat',
-      icon: <Ionicons name='library' size={30} color={disabledTextColor} style={{ marginEnd: 20 }} />,
-      disabled: true
+      icon: (
+        <ThemedView style={[styles.mainMenuItemIcon, { backgroundColor: 'dodgerblue' }]}>
+          <Ionicons name="library-outline" size={20} color={'white'} />
+        </ThemedView>
+      ),
+      endIcon: <SimpleLineIcons name="arrow-right" size={15} color={secondaryTextColor}
+                                style={styles.mainMenuItemEndIcon}/>,
     },
     {
       title: 'Selections',
       href: '/selections',
-      icon: <Ionicons name='bookmark' size={30} color={textColor} style={{ marginEnd: 20 }} />,
+      icon: (
+        <ThemedView style={[styles.mainMenuItemIcon, { backgroundColor: 'orange' }]}>
+          <Ionicons name="bookmark-outline" size={20} color={'white'} />
+        </ThemedView>
+      ),
+      endIcon: <SimpleLineIcons name="arrow-right" size={15} color={secondaryTextColor}
+                                style={styles.mainMenuItemEndIcon}/>,
     },
   ]
 
   return (
     <ThemedView style={styles.container}>
-      <Link href='/settings' asChild>
+      <Link href="/settings" asChild>
         <Pressable>
-          <AntDesign name='setting' size={30} color={textColor} style={styles.settingsIcon} />
+          <AntDesign name="setting" size={25} color={textColor} style={styles.settingsIcon}/>
         </Pressable>
       </Link>
       <Menu
         data={menuItems}
-        renderItem={({ item }) => <MenuItem {...item} fontSize={26} style={{ padding: 20 }} />}
+        renderItem={({ item }) => <MenuItem {...item} fontSize={26} style={styles.mainMenuItem}/>}
         keyExtractor={(item) => item.title}
+        style={styles.mainMenu}
       />
     </ThemedView>
   )
@@ -54,6 +79,25 @@ export default function HomeScreen () {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
+  },
+  mainMenu: {
+    paddingTop: 50
+  },
+  mainMenuItem: {
+    padding: 20,
+    marginHorizontal: 0
+  },
+  mainMenuItemIcon: {
+    marginEnd: 20,
+    width: 32,
+    height: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  mainMenuItemEndIcon: {
+    marginLeft: 'auto',
+    marginRight: 20,
   },
   settingsIcon: {
     alignSelf: 'flex-end',
