025b5e79c5c8ad9f6aa296b054502e68db54b54b
tombolton96 <tbolton96@icloud.com>
2024-08-17 17:05:40 +0100

    style surah name and reciter name in player

--- components/RecitationInfo.tsx
+++ components/RecitationInfo.tsx
@@ -4,6 +4,7 @@ import { ThemedText } from '@/components/ThemedText'
 import { NowPlayingStore } from '@/globalState/store'
 import { Suwar } from '@/constants/Suwar'
 import { observer } from 'mobx-react'
+import { useThemeColor } from '@/hooks/useThemeColor'
 
 type RecitationInfoProps = {
   isWidget?: boolean
@@ -11,15 +12,17 @@ type RecitationInfoProps = {
 }
 
 export const RecitationInfo = observer((props: RecitationInfoProps) => {
+  const secondaryTextColor = useThemeColor({}, 'secondaryText')
+
   return (
     <ThemedView style={props.isWidget ? styles.widgetContainer : styles.playerViewContainer}>
-      <ThemedText style={styles.surahName}>
+      <ThemedText style={[styles.surahName, !props.isWidget && styles.playerViewSurahName]}>
         {!props.nowPlaying.isLoading
           ? Suwar[props.nowPlaying.surahNumber - 1].name
           : '-' // TODO: loading component
         }
       </ThemedText>
-      <ThemedText>
+      <ThemedText style={[{ color: secondaryTextColor }, !props.isWidget && styles.playerViewReciterName]}>
         {!props.nowPlaying.isLoading
           ? props.nowPlaying.currentReciter?.name
           : '-' // TODO: loading component
@@ -35,6 +38,12 @@ const styles = StyleSheet.create({
   },
   surahName: {
     fontWeight: 'bold',
+  },
+  playerViewSurahName: {
+   fontSize: 20,
+  },
+  playerViewReciterName: {
+   fontSize: 18,
   },
   widgetContainer: {
     backgroundColor: 'transparent',
