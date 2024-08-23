3f771998697dd940db7958c91c4e2b24695e72bc
tombolton96 <tbolton96@icloud.com>
2024-08-23 15:50:23 +0100

    style screen headers

--- components/RootStack.tsx
+++ components/RootStack.tsx
@@ -10,16 +10,35 @@ type HomeScreenProps = {
 
 export const RootStack = inject('nowPlaying')(observer((props: HomeScreenProps) => {
   const tintColor = useThemeColor({}, 'tint')
+  const backgroundColor = useThemeColor({}, 'background')
 
   return (
     <ThemedSafeAreaView style={{ flex: 1 }}>
-      <Stack screenOptions={{ headerTintColor: tintColor }}>
-        <Stack.Screen name="index" options={{ headerShown: false }}/>
-        <Stack.Screen name="reciters" options={{ title: 'Reciters', headerBackTitleVisible: false }}/>
-        <Stack.Screen name="reciter/[id]"
-                      options={{ title: props.nowPlaying?.reciterPage?.name, headerBackTitleVisible: false }}/>
-        <Stack.Screen name="player" options={{ presentation: 'modal', headerShown: false }}/>
-        <Stack.Screen name="(settings)" options={{ presentation: 'modal', headerShown: false }}/>
+      <Stack screenOptions={{
+        headerTintColor: tintColor,
+        headerShadowVisible: false,
+        headerStyle: { backgroundColor },
+      }}>
+        <Stack.Screen
+          name="index"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="reciters"
+          options={{ title: 'Reciters', headerBackTitleVisible: false }}
+        />
+        <Stack.Screen
+          name="reciter/[id]"
+          options={{ title: props.nowPlaying?.reciterPage?.name, headerBackTitleVisible: false }}
+        />
+        <Stack.Screen
+          name="player"
+          options={{ presentation: 'modal', headerShown: false }}
+        />
+        <Stack.Screen
+          name="(settings)"
+          options={{ presentation: 'modal', headerShown: false }}
+        />
       </Stack>
     </ThemedSafeAreaView>
   )
