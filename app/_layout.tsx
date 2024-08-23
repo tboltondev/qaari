2c604505f1e070dc5081f533e97e6c5017d1a562
tombolton96 <tbolton96@icloud.com>
2024-08-23 15:21:10 +0100

    edit icons on home screen and add app name

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,20 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppThemeProvider } from '@/theme/AppThemeProvider'
 import { StoreProvider } from '@/globalState/StoreProvider'
 import { RootStack } from '@/components/RootStack'
+import { useFonts } from 'expo-font'
+import { SplashScreen } from 'expo-router'
 
 export default function RootLayout () {
   const queryClient = new QueryClient()
+  const [loaded] = useFonts({
+    Almarai: require('../assets/fonts/Almarai-Regular.ttf'),
+  })
+
+  React.useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded])
 
   return (
     <QueryClientProvider client={queryClient}>
