d82d15ef2e3e2b40f58ed254e81f63a53720dc4a
tombolton96 <tbolton96@icloud.com>
2024-08-24 15:22:54 +0100

    rename Player/Screen to Player/PlayerScreen

--- app/player.tsx
+++ app/player.tsx
@@ -1,4 +1,4 @@
-import { PlayerScreen } from '@/components/Player/Screen'
+import { PlayerScreen } from '@/components/Player/PlayerScreen'
 
 export default function Player () {
   return <PlayerScreen />
