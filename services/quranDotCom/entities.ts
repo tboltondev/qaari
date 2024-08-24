d309a2ceb59ab890b8a4acdff2ffe9868dccc3ee
tombolton96 <tbolton96@icloud.com>
2024-08-24 13:22:35 +0100

    created quran.com api service

---
+++ services/quranDotCom/entities.ts
@@ -0,0 +1,9 @@
+export type QuranDotComReciter = {
+  id: number
+  reciter_name: string
+  style: string
+  translated_name: {
+    name: string
+    language_name: string
+  }
+}
---
+++ services/quranDotCom/index.ts
@@ -0,0 +1,22 @@
+import { QuranDotComReciter } from '@/services/quranDotCom/entities'
+import { Reciter } from '@/domain/Reciter'
+
+const BASE_URL = 'https://api.quran.com/api/v4'
+
+const QuranDotCom = {
+  getReciters: async (): Promise<Reciter[]> => {
+    const response = await fetch(`${BASE_URL}/resources/recitations`)
+    const json = await response.json()
+    return json.recitations.map((reciter: QuranDotComReciter) => ({
+      id: reciter.id,
+      name: reciter.reciter_name,
+      style: reciter.style,
+      translatedName: {
+        name: reciter.translated_name.name,
+        languageName: reciter.translated_name.language_name,
+      },
+    }))
+  },
+}
+
+export default QuranDotCom
