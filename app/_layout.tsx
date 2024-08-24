6236b710b2072eb6a18419f8bdf79e40dc4eecc5
tombolton96 <tbolton96@icloud.com>
2024-08-24 11:07:55 +0100

    remove unused import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ import { AppThemeProvider } from '@/theme/AppThemeProvider'
 import { StoreProvider } from '@/globalState/StoreProvider'
 import { RootStack } from '@/components/RootStack'
 import { useFonts } from 'expo-font'
-import { SplashScreen } from 'expo-router'
 
 export default function RootLayout () {
   const queryClient = new QueryClient()
